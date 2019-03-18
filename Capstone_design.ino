f3dd62d38668cc6671eaf9c14213b5eb37d11af3
micaellajimini <jimin7971@gmail.com>
2019-03-18 19:35:55 +0900

    send data to server

--- Capstone_design.ino
+++ Capstone_design.ino
@@ -8,6 +8,7 @@
 // the number can be activated is case by case
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
+String apiKey = "MGT3HJLAUHRHOVDX"; // to be changed
 ESP8266WiFiMulti WiFiMulti;
 
 void setup() {
@@ -38,37 +39,52 @@ void setup() {
   sensors.begin();
 }
 
-
-void loop() {
-  const uint16_t port = 80;
-  const char * host = "api.thingspeak.com"; // ip or dns
-  String apiKey = "MGT3HJLAUHRHOVDX"; // to be changed
-  // make account on thingspeak
-  // write apikey of channel
-
+void senddata(int flag) {
   WiFiClient client;
-
-  if (!client.connect(host, port)) {
-    Serial.println("connection failed");
-    return;
-  }
   sensors.requestTemperatures();
   Serial.print("Temperature for Device 1 is: ");
   String temp = String(sensors.getTempCByIndex(0));
   Serial.println(temp);
   delay(500);
-  String url = "/update?api_key=" + apiKey + "&field1=" + temp;
+  const char * host; // ip or dns
+  String url;
+  uint16_t port;
+
+  if(flag==0){
+    host="api.thingspeak.com";
+    port = 80;
+    url = "/update?api_key=" + apiKey + "&field1=" + temp;
+  }
+  if(flag==1){
+      host = "52.79.239.221";
+      port = 8000;
+      url = "/log?temp=" + temp; 
+  }
+  if (!client.connect(host, port)) {
+    Serial.println("connection failed");
+    return;
+  }
+  if(flag==1) {
+    host = "http://52.79.239.221";
+  }
   client.print(String("GET ") + url + " HTTP/1.1\r\n" +
                "Host: " + host + "\r\n" +
                "Connection: close\r\n\r\n");
-  delay(60000); // should request after 15 seconds
-  int timeout = millis() + 20000;
-  while (client.available() == 0) {
-    if (timeout - millis() < 0) {
-      Serial.println(">>> Client Timeout !");
-      client.stop();
-      return;
-    }
+  Serial.println(host);
+  delay(1000); // should request after 15 seconds
+}
+
+void loop() {
+  // make account on thingspeak
+  // write apikey of channel
+
+  static long millisSec = millis();
+
+  if (millis() - millisSec > 60000) {
+    millisSec = millis();
+    senddata(0);
+
+    senddata(1);
   }
 }
 
